5a8af87bbeaa371849d1ad342b361c1a18f71cef
La Quốc Thắng <quocthang0507@gmail.com>
2020-12-16 18:38:01 +0700

    Update resource

--- QuanLyDienNang/Properties/Resources.Designer.cs
+++ QuanLyDienNang/Properties/Resources.Designer.cs
@@ -113,16 +113,6 @@ namespace QuanLyDienNang.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap anh_nen_4 {
-            get {
-                object obj = ResourceManager.GetObject("anh_nen_4", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Archive {
             get {
                 object obj = ResourceManager.GetObject("Archive", resourceCulture);
